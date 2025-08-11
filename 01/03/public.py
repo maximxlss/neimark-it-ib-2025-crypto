e4d328348100ead38df5d554b1cb10e451bd82d2
maximxls <maximxlssoff@gmail.com>
2025-08-11 12:47:21 +0300

    fix 0103 second root

--- 01/03/public.py
+++ 01/03/public.py
@@ -10,7 +10,7 @@ y = (x ** 2 + 7 * x + 2) % p
 print(f"{y = }")
 
 guess = int(input("? x = "))
-if guess == x or guess == -x % p:
+if guess == x or guess == (-7 - x) % p:
     print("neimark{REDACTEDREDACTEDREDACTEDREDACTED}")
 else:
     print(f"Wrong! {x = }")
--- 01/03/task.py
+++ 01/03/task.py
@@ -10,7 +10,7 @@ y = (x ** 2 + 7 * x + 2) % p
 print(f"{y = }")
 
 guess = int(input("? x = "))
-if guess == x or guess == -x % p:
+if guess == x or guess == (-7 - x) % p:
     print("neimark{pr0st0_v0sym0y_cl455_44fb89c7}")
 else:
     print(f"Wrong! {x = }")
